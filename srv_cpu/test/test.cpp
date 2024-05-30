f0d06f6d7b1a6952c83e316b41150d4f9a385104
ycl <931415151@qq.com>
2024-05-30 20:33:58 +0800

    del unused code

--- srv_cpu/test/test.cpp
+++ srv_cpu/test/test.cpp
@@ -12,53 +12,8 @@ TEST(AddTest, Basic) {
   EXPECT_EQ(Add(1, 2), 3);
 }
 
-// Function to perform diagonal propagation on a matrix
-void diagonalPropagation(vector<vector<double>> &matrix) {
-    int n = matrix.size(); // Size of the matrix (n x n)
-
-    // Iterate over the diagonal elements
-    for (int i = 0; i < n; i++) {
-        double diagonalElement = matrix[i][i]; // Current diagonal element
-
-        // Propagate the diagonal element to the remaining elements in the row and column
-        for (int j = 0; j < n; j++) {
-            if (i != j) {
-                matrix[i][j] -= (matrix[i][0] * matrix[j][0]) / diagonalElement;
-                matrix[j][i] -= (matrix[0][i] * matrix[0][j]) / diagonalElement;
-            }
-        }
-    }
-}
-
-// Function to print the matrix
-void printMatrix(const vector<vector<double>> &matrix) {
-    int n = matrix.size();
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
 void TEST_dpa()
 {
-    /*
-    // Example matrix
-    vector<vector<double>> matrix = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
-    };
-
-    // Perform diagonal propagation
-    diagonalPropagation(matrix);
-
-    // Print the modified matrix
-    cout << "Modified matrix:" << endl;
-    printMatrix(matrix);
-    */
-
     // Test case 1: Basic functionality
     std::cout << "Test case 1: Basic functionality" << std::endl;
     dpa_scheduler arbiter1(4, 4);
