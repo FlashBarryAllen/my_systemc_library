ef19705c6a552d6351012a01998d3400f931da1c
ycl <931415151@qq.com>
2024-11-14 16:44:22 +0800

    format

--- srv_cpu/main.cpp
+++ srv_cpu/main.cpp
@@ -1,5 +1,5 @@
-#include "top.h"
 #include "gtest/gtest.h"
+#include "top.h"
 using ::testing::EmptyTestEventListener;
 using ::testing::InitGoogleTest;
 using ::testing::Test;
@@ -8,16 +8,14 @@ using ::testing::TestInfo;
 using ::testing::TestPartResult;
 using ::testing::UnitTest;
 
-
-int sc_main(int argc, char* argv[])
-{
+int sc_main(int argc, char* argv[]) {
     InitGoogleTest(&argc, argv);
     int ret_val = RUN_ALL_TESTS();
 
     std::cout << "hello server cpu" << std::endl;
     TEST_dpa();
 
-    //TEST_islip();
+    // TEST_islip();
     TEST_encap_pkt();
 
     TEST_sch();
@@ -36,6 +34,6 @@ int sc_main(int argc, char* argv[])
     sc_start(10, sc_core::SC_NS);
 
     std::cout << "done" << std::endl;
-    
+
     return 0;
 }
--- srv_cpu/top.h
+++ srv_cpu/top.h
@@ -2,17 +2,14 @@
 #define TOP_H
 
 #include <memory>
-#include "src.h"
-#include "tgt.h"
 #include "islip.h"
+#include "src.h"
 #include "test.h"
+#include "tgt.h"
 
-class my_src : public src
-{
-public:
-    my_src(sc_core::sc_module_name name) : src(name) {
-
-    }
+class my_src : public src {
+   public:
+    my_src(sc_core::sc_module_name name) : src(name) {}
     /*
     virtual void mth_entry() {
         std::cout << "my_src" << std::endl;
@@ -20,75 +17,69 @@ public:
     */
 };
 
-class top : public sc_core::sc_module
-{
-    public:
-        SC_HAS_PROCESS(top);
-        top(sc_core::sc_module_name name) {
-            m_clk = new sc_core::sc_clock("clk", 1, sc_core::SC_NS);
-            s = std::make_shared<my_src>("my_src");
-            t = std::make_shared<tgt>("tgt");
-            s->m_clk(*m_clk);
-            t->m_clk(*m_clk);
-            s->m_src_sk.bind(t->m_tgt_sk);
-            s->m_credit = 2;
-        }
+class top : public sc_core::sc_module {
+   public:
+    SC_HAS_PROCESS(top);
+    top(sc_core::sc_module_name name) {
+        m_clk = new sc_core::sc_clock("clk", 1, sc_core::SC_NS);
+        s = std::make_shared<my_src>("my_src");
+        t = std::make_shared<tgt>("tgt");
+        s->m_clk(*m_clk);
+        t->m_clk(*m_clk);
+        s->m_src_sk.bind(t->m_tgt_sk);
+        s->m_credit = 2;
+    }
 
-    public:
-        sc_core::sc_clock* m_clk;
-        std::shared_ptr<my_src> s;
-        std::shared_ptr<tgt> t;
+   public:
+    sc_core::sc_clock* m_clk;
+    std::shared_ptr<my_src> s;
+    std::shared_ptr<tgt> t;
 };
 
-class A : public sc_core::sc_module
-{
-    public:
-        sc_core::sc_in_clk clock;
-        sc_fifo_out<int>   tx;
-        int                cnt = 0;
-    public:
-        SC_HAS_PROCESS(A);
-        A(sc_core::sc_module_name name)
-        {
-            SC_METHOD(mth_entry);
-            sensitive << clock.pos();
-        }
+class A : public sc_core::sc_module {
+   public:
+    sc_core::sc_in_clk clock;
+    sc_fifo_out<int> tx;
+    int cnt = 0;
 
-        void mth_entry()
-        {
-            cnt++;
-            static int i = 0;
-            tx->nb_write(i);
-            std::cout << "cnt: " << cnt << ", AA: " << i << std::endl;
-            i++;
-        }
+   public:
+    SC_HAS_PROCESS(A);
+    A(sc_core::sc_module_name name) {
+        SC_METHOD(mth_entry);
+        sensitive << clock.pos();
+    }
+
+    void mth_entry() {
+        cnt++;
+        static int i = 0;
+        tx->nb_write(i);
+        std::cout << "cnt: " << cnt << ", AA: " << i << std::endl;
+        i++;
+    }
 };
 
-class BB : public sc_core::sc_module
-{
-    public:
-        sc_core::sc_in_clk clock;
-        sc_fifo_in<int>   rx;
-        int               cnt = 0;
-    public:
-        SC_HAS_PROCESS(BB);
-        BB(sc_core::sc_module_name name)
-        {
-            SC_METHOD(mth_entry);
-            sensitive << clock.pos();
-        }
+class BB : public sc_core::sc_module {
+   public:
+    sc_core::sc_in_clk clock;
+    sc_fifo_in<int> rx;
+    int cnt = 0;
 
-        void mth_entry()
-        {
-            cnt++;
-            int i = 0;
-            while (rx->num_available())
-            {
-                //rx->nb_read(i);
-                i = rx->read();
-                std::cout << "cnt: " << cnt << ", BB: " << i << std::endl;
-            }
+   public:
+    SC_HAS_PROCESS(BB);
+    BB(sc_core::sc_module_name name) {
+        SC_METHOD(mth_entry);
+        sensitive << clock.pos();
+    }
+
+    void mth_entry() {
+        cnt++;
+        int i = 0;
+        while (rx->num_available()) {
+            // rx->nb_read(i);
+            i = rx->read();
+            std::cout << "cnt: " << cnt << ", BB: " << i << std::endl;
         }
+    }
 };
 
 #endif
