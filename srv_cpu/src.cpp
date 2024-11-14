661442d15bb3e8869a0e4ecd05d02d007bac1d14
ycl <931415151@qq.com>
2024-11-14 15:30:09 +0800

    cpplint format

--- srv_cpu/src.cpp
+++ srv_cpu/src.cpp
@@ -1,6 +1,7 @@
 #include "src.h"
 
-src::src(sc_core::sc_module_name name) : m_src_sk("src_sk"), m_val(0), m_credit(0) {
+src::src(sc_core::sc_module_name name)
+    : m_src_sk("src_sk"), m_val(0), m_credit(0) {
     m_src_sk.register_nb_transport_bw(this, &src::nb_transport_bw);
     SC_METHOD(mth_entry);
     sensitive << m_clk.pos();
@@ -8,13 +9,14 @@ src::src(sc_core::sc_module_name name) : m_src_sk("src_sk"), m_val(0), m_credit(
 
     m_cycle_cnt = 0;
 
-    m_logger = spdlog::basic_logger_mt(this->name(), std::string("logs/") + this->name() + ".log", true);
+    m_logger = spdlog::basic_logger_mt(
+        this->name(), std::string("logs/") + this->name() + ".log", true);
     m_logger->set_level(spdlog::level::info);
     m_logger->set_pattern("[%Y-%m-%d %H:%M:%S] %v");
 }
 
-void src::mth_entry() {  
-    m_cycle_cnt++;  
+void src::mth_entry() {
+    m_cycle_cnt++;
     cal_speed();
 
     auto p_api = new MY_API_T();
@@ -24,7 +26,7 @@ void src::mth_entry() {
     p_dat->a = 10 + m_val;
     p_dat->b = 'a';
     p_dat->c = 101;
-    
+
     p_api->dat = p_dat;
 
     m_logger->info("SRC print a: 0x{:x}", p_dat->a);
@@ -49,29 +51,28 @@ void src::cal_speed() {
     std::cout << "当前时间：" << ts.tv_sec << "." << ts.tv_nsec << std::endl;
 
     double cur_us = sc_core::sc_time_stamp().to_seconds() * 10e6;
-    
+
     std::cout << cur_us << std::endl;
 }
 
-tlm::tlm_sync_enum src::nb_transport_bw(tlm::tlm_generic_payload& trans, tlm::tlm_phase& phase,
-    sc_core::sc_time& time) {
-        m_logger->info("[SRC][RCV] cycle: {:d}", m_cycle_cnt);
+tlm::tlm_sync_enum src::nb_transport_bw(tlm::tlm_generic_payload& trans,
+                                        tlm::tlm_phase& phase,
+                                        sc_core::sc_time& time) {
+    m_logger->info("[SRC][RCV] cycle: {:d}", m_cycle_cnt);
 
-        auto p_api = (MY_API_T*)trans.get_data_ptr();
-        auto p_dat = static_pointer_cast<MY_DAT_T>(p_api->dat);
-        auto type = p_api->msg.type;
+    auto p_api = (MY_API_T*)trans.get_data_ptr();
+    auto p_dat = static_pointer_cast<MY_DAT_T>(p_api->dat);
+    auto type = p_api->msg.type;
 
-        if (type == CTRL_MSG) {
-            m_logger->info("[SRC][RCV] credit: {:d}", p_api->msg.credit);
-            m_credit += p_api->msg.credit;
-        } else if (type == DATA_MSG) {
-            m_logger->info("[SRC][RCV] a: {:d}", p_dat->a);
-        }
+    if (type == CTRL_MSG) {
+        m_logger->info("[SRC][RCV] credit: {:d}", p_api->msg.credit);
+        m_credit += p_api->msg.credit;
+    } else if (type == DATA_MSG) {
+        m_logger->info("[SRC][RCV] a: {:d}", p_dat->a);
+    }
 
-        delete p_api;
-        return tlm::TLM_COMPLETED; 
+    delete p_api;
+    return tlm::TLM_COMPLETED;
 }
 
-void src::set_credit(int credit) {
-    m_credit = credit;
-}
+void src::set_credit(int credit) { m_credit = credit; }
--- srv_cpu/tgt.cpp
+++ srv_cpu/tgt.cpp
@@ -1,19 +1,18 @@
 #include "tgt.h"
 
-tgt::tgt(sc_core::sc_module_name name) : m_crdt_sch_cycle(2)
-    , m_cycle_cnt(0) {
+tgt::tgt(sc_core::sc_module_name name) : m_crdt_sch_cycle(2), m_cycle_cnt(0) {
     m_tgt_sk.register_nb_transport_fw(this, &tgt::nb_transport_fw);
     SC_METHOD(mth_entry);
     sensitive << m_clk.pos();
     dont_initialize();
 
-    m_logger = spdlog::basic_logger_mt(this->name(), std::string("logs/") + this->name() + ".log", true);
+    m_logger = spdlog::basic_logger_mt(
+        this->name(), std::string("logs/") + this->name() + ".log", true);
     m_logger->set_level(spdlog::level::info);
     m_logger->set_pattern("[%Y-%m-%d %H:%M:%S] %v");
 }
 
-void tgt::mth_dat_proc()
-{
+void tgt::mth_dat_proc() {
     if (m_rcv_dat.size() != 0) {
         auto p_dat = m_rcv_dat.front();
         m_peq.delay(*p_dat, 3);
@@ -28,7 +27,7 @@ void tgt::mth_dat_proc()
 void tgt::mth_get_peq() {
     shared_ptr<MY_DAT_T> p_dat = nullptr;
     while ((p_dat = m_peq.get_next_transaction()) != nullptr) {
-        //std::cout << "sc_time_stamp: " << sc_time_stamp() << std::endl;
+        // std::cout << "sc_time_stamp: " << sc_time_stamp() << std::endl;
         m_peq_que.push_back(p_dat);
     }
 }
@@ -45,7 +44,7 @@ void tgt::mth_snd_msg() {
         trans.set_data_ptr((unsigned char*)p_api);
         tlm::tlm_phase phase = tlm::BEGIN_REQ;
         sc_time time = SC_ZERO_TIME;
-        
+
         m_tgt_sk->nb_transport_bw(trans, phase, time);
 
         m_snd_ctl.pop_front();
@@ -65,12 +64,11 @@ void tgt::mth_snd_msg() {
         trans.set_data_ptr((unsigned char*)p_api);
         tlm::tlm_phase phase = tlm::BEGIN_REQ;
         sc_time time = SC_ZERO_TIME;
-        
+
         auto status = m_tgt_sk->nb_transport_bw(trans, phase, time);
         if (status == tlm::TLM_COMPLETED) {
             m_snd_msg_que.pop_front();
         } else {
-
         }
 
         m_logger->info("[TGT][SND] cycle: {:d}", m_cycle_cnt);
@@ -85,35 +83,30 @@ void tgt::mth_entry() {
     mth_snd_msg();
 }
 
-tlm::tlm_sync_enum tgt::nb_transport_fw(tlm::tlm_generic_payload& trans, tlm::tlm_phase& phase,
-    sc_core::sc_time& time) {
-        auto p_api = (MY_API_T*)trans.get_data_ptr();
-        auto p_dat = std::static_pointer_cast<MY_DAT_T>(p_api->dat);
-        m_rcv_dat.push_back(p_dat);
-        delete p_api;
+tlm::tlm_sync_enum tgt::nb_transport_fw(tlm::tlm_generic_payload& trans,
+                                        tlm::tlm_phase& phase,
+                                        sc_core::sc_time& time) {
+    auto p_api = (MY_API_T*)trans.get_data_ptr();
+    auto p_dat = std::static_pointer_cast<MY_DAT_T>(p_api->dat);
+    m_rcv_dat.push_back(p_dat);
+    delete p_api;
 
-        m_logger->info("[TGT][RCV] cycle: {:d}", m_cycle_cnt);
-        return tlm::TLM_COMPLETED;
+    m_logger->info("[TGT][RCV] cycle: {:d}", m_cycle_cnt);
+    return tlm::TLM_COMPLETED;
 }
 
-B::B(sc_core::sc_module_name name)
-{
+B::B(sc_core::sc_module_name name) {
     m_tlm_if.register_nb_transport_fw(this, &B::nb_transport_fw);
     SC_METHOD(mth_entry);
     sensitive << m_clk.pos();
 }
 
-B::~B()
-{
+B::~B() {}
 
-}
+void B::mth_entry() { return; }
 
-void B::mth_entry()
-{
-    return;
-}
-
-tlm::tlm_sync_enum B::nb_transport_fw(tlm::tlm_generic_payload& trans, tlm::tlm_phase& phase,
-    sc_core::sc_time& time) {
-        return tlm::TLM_COMPLETED;
+tlm::tlm_sync_enum B::nb_transport_fw(tlm::tlm_generic_payload& trans,
+                                      tlm::tlm_phase& phase,
+                                      sc_core::sc_time& time) {
+    return tlm::TLM_COMPLETED;
 }
